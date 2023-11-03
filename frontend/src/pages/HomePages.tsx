11dece19364c6de0c420bb74cd9d1f2d91d18cbe
GuilhermeMoura <guilherme.moura@sou.inteli.edu.br>
2023-11-03 05:55:31 -0300

    feat: add news card

--- frontend/src/pages/HomePages.tsx
+++ frontend/src/pages/HomePages.tsx
@@ -1,7 +1,12 @@
+import PostCard from "@/components/postcard";
+
 export default function HomePage(){
     return (
-        <div>
+        <><div>
             <h1>Home dsdadasdadasPage</h1>
         </div>
+        <div>
+            <PostCard />
+        </div></>
     )
 }
