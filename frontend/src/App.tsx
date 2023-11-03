cf5c189857b43119d58ccf02b40dab59793cdcf4
Samuel Lucas de Almeida <samuel.almeida@sou.inteli.edu.br>
2023-11-03 01:49:19 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { ModeToggle } from './components/mode-toggle'
 import { ThemeProvider } from './components/theme-provider'
 
 function App({ children }: { children: React.ReactNode }) {
 
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      {children}
+      <ModeToggle />
     </ThemeProvider>
   )
 }
