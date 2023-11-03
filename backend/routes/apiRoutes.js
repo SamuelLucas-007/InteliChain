724db5713e49c7244e8edc64e724b1d532fdf080
Samuel Lucas de Almeida <samuel.almeida@sou.inteli.edu.br>
2023-11-03 01:46:44 -0300

    Iniciando front

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
-const { fetchWalletInfo, createInvoice, payInvoice } = require('../controllers/apiController');
+const { fetchWalletInfo, createInvoice, payInvoice, fetchWalletInfoAlby, createInvoiceAlby, payInvoiceAlby } = require('../controllers/apiController');
 
 router.get('/wallet-info', fetchWalletInfo);
+router.get('/wallet-info-alby', fetchWalletInfo);
 
 router.post('/invoice', createInvoice);
+router.post('/invoice-alby', createInvoiceAlby);
 
 router.post('/pay-invoice', payInvoice);
+router.post('/pay-invoice-alby', payInvoiceAlby);
 
 module.exports = router;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ThemeProvider } from './components/theme-provider'
+
+function App({ children }: { children: React.ReactNode }) {
+
+  return (
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export default App
