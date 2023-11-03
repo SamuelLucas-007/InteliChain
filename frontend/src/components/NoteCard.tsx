b4897a2a2ebdb626a09d6250915904c89b41ddd9
YagoDevs <110265917+YagoDevs@users.noreply.github.com>
2023-11-03 16:14:56 -0300

    atualizando
    atualizando branch

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -1,24 +1,28 @@
 import { object } from "prop-types";
 import { useState } from "react";
+import comment from "../assets/message-circle.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import zap from "../assets/zap.svg";
+import eye from "../assets/eye.svg";
 
 interface Props {
-	content: string;
-	user: {
-		name: string;
-		image: string;
-		pubkey: string;
-	};
-	created_at: number;
-	hashtags: string[];
+  content: string;
+  user: {
+    name: string;
+    image: string;
+    pubkey: string;
+  };
+  created_at: number;
+  hashtags: string[];
 }
 
 export default function NoteCard({
-	content,
-	user,
-	created_at,
-	hashtags,
+  content,
+  user,
+  created_at,
+  hashtags,
 }: Props) {
-
   const texto = content;
 
   // Encontrando os índices para cada parte dos dados
@@ -37,96 +41,83 @@ export default function NoteCard({
   const tagsInicio = texto.indexOf("Tags:") + 6;
   const tagsFim = texto.length;
 
-    // Extraindo as substrings com base nos índices encontrados
-    const title = texto.substring(tituloInicio, tituloFim).trim();
-    const image = texto.substring(imagemInicio, imagemFim).trim();
-    const conteudo = texto.substring(conteudoInicio, conteudoFim).trim();
-    const wallet = texto.substring(walletInicio, walletFim).trim();
-    const tags = texto.substring(tagsInicio, tagsFim).trim();
+  // Extraindo as substrings com base nos índices encontrados
+  const title = texto.substring(tituloInicio, tituloFim).trim();
+  const image = texto.substring(imagemInicio, imagemFim).trim();
+  const conteudo = texto.substring(conteudoInicio, conteudoFim).trim();
+  const wallet = texto.substring(walletInicio, walletFim).trim();
+  const tags = texto.substring(tagsInicio, tagsFim).trim();
 
-    if(title && image && conteudo && wallet && tags && tituloInicio >= 7 && imagemInicio >= 7 && conteudoInicio >= 9 && walletInicio >= 8 && tagsInicio >= 6 ) {
-      return (
-      
-        <div className="max-w-xl rounded p-16 border border-gray-600 bg-gray-700 flex flex-col gap-16 break-words">
-          <div className="flex gap-12 items-center overflow-hidden">
-            <img
-              src={user.image}
-              alt="note"
-              className="rounded-full w-32 aspect-square bg-gray-100"
-            />
-            <div>
-              <a
-                href={`https://nostr.guru/p/${user.pubkey}`}
-                className="text-body3 text-white overflow-hidden text-ellipsis"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {user.name}
-              </a>
-              <p className="text-body5 text-gray-400">
-                {new Date(created_at * 1000).toISOString().split("T")[0]}
-              </p>
-            </div>
+  if (title && image && conteudo && wallet && tags && tituloInicio >= 7 && imagemInicio >= 7 && conteudoInicio >= 9 && walletInicio >= 8 && tagsInicio >= 6) {
+    return (
+      <div className="max-w-xl rounded p-16 border border-gray-600 bg-white flex flex-col gap-16 break-words">
+        <div className="flex gap-12 items-center overflow-hidden">
+          <img
+            src={user.image}
+            alt="note"
+            className="rounded-full w-32 aspect-square bg-gray-100"
+          />
+          <div>
+            <a
+              href={`https://nostr.guru/p/${user.pubkey}`}
+              className="text-body3 text-white overflow-hidden text-ellipsis"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {user.name}
+            </a>
+            <p className="text-body5 text-gray-400">
+              {new Date(created_at * 1000).toISOString().split("T")[0]}
+            </p>
           </div>
-          <h1>{title}</h1>
-
-          <img src={image} alt="F dmss" />
-
-          <p>{conteudo}</p>
-
-          <p>{wallet}</p>
-
-          <ul className="flex flex-wrap gap-12">
-            {hashtags
-              .filter((t) => hashtags.indexOf(t) === hashtags.lastIndexOf(t))
-              .map((hashtag) => (
-                <li
-                  key={hashtag}
-                  className="bg-gray-300 text-body5 text-gray-900 font-medium rounded-24 px-12 py-4"
-                >
-                  #{hashtag}
-                </li>
-              ))}
-          </ul>
         </div>
-      );
-    } else {
-      return (
-          <div className="max-w-xl rounded p-16 border border-gray-600 bg-gray-700 flex flex-col gap-16 break-words">
-            <div className="flex gap-12 items-center overflow-hidden">
-              <img
-                src={user.image}
-                alt="note"
-                className="rounded-full w-32 aspect-square bg-gray-100"
-              />
-              <div>
-                <a
-                  href={`https://nostr.guru/p/${user.pubkey}`}
-                  className="text-body3 text-white overflow-hidden text-ellipsis"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {user.name}
-                </a>
-                <p className="text-body5 text-gray-400">
-                  {new Date(created_at * 1000).toISOString().split("T")[0]}
-                </p>
-              </div>
-            </div>
-            {content}
-            <ul className="flex flex-wrap gap-12">
-              {hashtags
-                .filter((t) => hashtags.indexOf(t) === hashtags.lastIndexOf(t))
-                .map((hashtag) => (
-                  <li
-                    key={hashtag}
-                    className="bg-gray-300 text-body5 text-gray-900 font-medium rounded-24 px-12 py-4"
-                  >
-                    #{hashtag}
-                  </li>
-                ))}
-            </ul>
+        <h1>{title}</h1>
+        <img src={image} alt="F dmss" />
+        <p className="text-left">{conteudo}</p>
+        <p>{wallet}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="max-w-xl rounded  bg-white flex flex-col gap-16 break-words">
+        <div className="flex gap-12 items-center overflow-hidden">
+          <img
+            src={user.image}
+            alt="note"
+            className="rounded-full w-12 aspect-square bg-gray-100"
+          />
+          <div>
+            <a
+              href={`https://nostr.guru/p/${user.pubkey}`}
+              className="text-body3 font-inter font-bold text-black overflow-hidden text-ellipsis"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {user.name}
+            </a>
+            <p className="text-body5 text-gray-400">
+              {new Date(created_at * 1000).toISOString().split("T")[0]}
+            </p>
           </div>
-        );
-	}
+        </div>
+        <p className="-translate-y-10 text-left">{content}</p>
+        <div className="flex justify-between -translate-y-20">
+          <div className="flex align-center gap-7">
+            <img src={comment} width="35" height="35"></img>
+            <img src={like} width="30" height="30"></img>
+            <img src={dislike} width="30" height="30"></img>
+          </div>
+          <div className="flex gap-10">
+            <button className="border-radius-post-buttons pt-3 pb-3 pl-9 pr-9 orange-chain">
+              <img src={zap} width="25" height="25" />
+            </button>
+            <button className="border-radius-post-buttons pt-3 pb-3 pl-9 pr-9 orange-chain">
+              <img src={eye} width="25" height="25" />
+            </button>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
 }
