c38ae60fc78b7dfcd04f22a4b1959dabcbe52320
Arthur Tsuka <arthur.oliveira@sou.inteli.edu.br>
2023-11-03 04:24:22 -0300

    a

--- frontend/src/pages/HomePages.tsx
+++ frontend/src/pages/HomePages.tsx
@@ -1,7 +1,27 @@
-export default function HomePage(){
-    return (
-        <div>
-            <h1>Home dsdadasdadasPage</h1>
-        </div>
-    )
+import Ranking from "@/components/ranking/Ranking";
+
+const data = [
+    {
+      name: "MaceloMaia?",
+      chavePublica: "bc1p2...72lnnk",
+      image: "../../",
+    },
+    {
+      name: "OutroMaceloMaia",
+      chavePublica: "bc1p2...72lnnk",
+      image: "",
+    },
+    {
+      name: "OMaceloMaia",
+      chavePublica: "bc1p2...72lnnk",
+      image: "",
+    },
+  ];
+
+export default function HomePage() {
+  return (
+    <div>
+      <Ranking data={data} />
+    </div>
+  );
 }
