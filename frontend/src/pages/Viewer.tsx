ad952c18902f2cbb13e502f9a870f85f40f2ef12
marcelomaiaf <marcelo.filho@sou.inteli.edu.br>
2023-11-03 14:47:51 -0300

    commitando bitcoin

--- frontend/src/pages/Viewer.tsx
+++ frontend/src/pages/Viewer.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { useGLTF, OrbitControls } from '@react-three/drei';
 import { Group } from 'three';
 
@@ -7,19 +7,36 @@ function Model() {
   const gltf = useGLTF('scene.gltf');
   const group = useRef<Group>();
 
+  // Adicione um estado para controlar a rotação
+  const rotationSpeed = 0.005;
+
+  useFrame(() => {
+    // Atualize a rotação do grupo a cada quadro
+    group.current.rotation.y += rotationSpeed;
+  });
+
   return <primitive object={gltf.scene} ref={group} />;
 }
 
 function Viewer() {
   return (
     <div className='w-full h-full'>
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} intensity={30}/>
-      <pointLight position={[10, 10, 10]} />
-      <Model />
-      <OrbitControls />
-    </Canvas>
+      <Canvas>
+        <ambientLight intensity={3} />
+        <directionalLight position={[-200, 20, 5]} intensity={50} />
+        <pointLight position={[-5, -10, 200]} />
+        <Model />
+        <OrbitControls
+          enableZoom={true} // Permitir zoom
+          enablePan={true} // Permitir panorâmica
+          enableRotate={true} // Permitir rotação
+          minPolarAngle={Math.PI / 8} // Ângulo mínimo de rotação vertical
+          maxPolarAngle={Math.PI / 1} // Ângulo máximo de rotação vertical
+          minDistance={10} // Distância mínima da câmera
+          maxDistance={200} // Distância máxima da câmera
+          target={[100, 40, 0]} // Alvo da câmera (posição inicial)
+        />
+      </Canvas>
     </div>
   );
 }
